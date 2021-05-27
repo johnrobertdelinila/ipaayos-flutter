eb0b5f88fbed01155ef3d7869a6e870d61ec460d
John Robert Delinila <36176710+johnrobertdelinila@users.noreply.github.com>
2021-05-28 06:14:48 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/uclab/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yqgon.flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/providers/contants.dart
@@ -0,0 +1,5 @@
+
+// ignore: non_constant_identifier_names
+const String static_url = "192.168.254.147"           + "/";
+const String file_name = "ipaayos-admin"              + "/";
+const String base_url = 'http://' + static_url + file_name + 'webservice';
